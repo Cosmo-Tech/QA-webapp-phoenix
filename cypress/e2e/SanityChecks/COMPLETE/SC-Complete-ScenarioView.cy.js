95b3a006bc57f5c3776b866bc3b51427ddb7222b
fanny.silencieux <fanny.silencieux@cosmotech.com>
2025-05-21 15:54:47 +0200

    Fix function path that were not good anymore after moving the test files

--- cypress/e2e/SanityChecks/COMPLETE/SC-Complete-ScenarioView.cy.js
+++ cypress/e2e/SanityChecks/COMPLETE/SC-Complete-ScenarioView.cy.js
@@ -1,8 +1,8 @@
 import 'cypress-file-upload';
-const connection = require('../../functions/connect.cy.js');
-const datasetManager = require('../../functions/datasetManager.cy.js');
-const config = require('../../../variables.cy.js');
-const scenario = require('../../functions/scenario.cy.js');
+const connection = require('../../../functions/connect.cy.js');
+const datasetManager = require('../../../functions/datasetManager.cy.js');
+const config = require('../../../../variables.cy.js');
+const scenario = require('../../../functions/scenario.cy.js');
 
 describe('Scenario View feature', () => {
   it('PROD-11815 -> Validate and Reject scenario', () => {
